c615b9ae3f6b715010170c6146d230dc82c493eb
HuangYao <huangyao@ezbuy.com>
2018-07-25 17:02:52 +0800

    [update]Paginator done

--- static/js/custom/bar.js
+++ static/js/custom/bar.js
@@ -9,8 +9,6 @@ $('.nav-second-level').find('a').each(function () {
     for (let i = 0; i < 5; i++){
         new_url = new_url + href_list[i] + '/'
     }
-    console.log(new_url);
-    console.log('========================');
     if (this.href == document.location.href || document.location.href.search(this.href) >= 0 || document.location.href.search(new_url) >= 0) {
         $(this).parent().addClass('active'); // this.className = 'active';
         $(this).parent().parent().parent().addClass('active');
